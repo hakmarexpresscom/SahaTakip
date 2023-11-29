59ef90314094bb4e3e8715cc1ed994f595876055
Elif Ünlü <elif.unlu@hakmarekspres.corp>
2023-11-29 17:01:29 +0300

    çelik kasa sayımı model ve servisleri

---
+++ lib/models/cashCounting.dart
@@ -0,0 +1,46 @@
+class CashCounting{
+  late int shopCode;
+  late int? bs_id;
+  late int? pm_id;
+  late String kayit_tarihi;
+  late String kagit_para_sayimi;
+  late String madeni_para_sayimi;
+  late String poslar_toplami;
+  late String masraflar_tediyeler;
+  late String celik_kasa_mevcudu;
+  late String kasa_defter_mevcudu;
+  late String fark;
+
+  CashCounting({
+    required this.shopCode,
+    required this.bs_id,
+    required this.pm_id,
+    required this.kayit_tarihi,
+    required this.kagit_para_sayimi,
+    required this.madeni_para_sayimi,
+    required this.poslar_toplami,
+    required this.masraflar_tediyeler,
+    required this.celik_kasa_mevcudu,
+    required this.kasa_defter_mevcudu,
+    required this.fark
+  });
+
+  factory CashCounting.fromJson(Map<String, dynamic> json) {
+    return CashCounting(
+      shopCode: json['magaza_kodu'],
+      bs_id: json['bs_id'],
+      pm_id: json['pm_id'],
+      kayit_tarihi: json['kayit_tarihi'],
+      kagit_para_sayimi: json['kagit_para_sayimi'],
+      madeni_para_sayimi: json['madeni_para_sayimi'],
+      poslar_toplami: json['poslar_toplami'],
+      masraflar_tediyeler: json['masraflar_tediyeler'],
+      celik_kasa_mevcudu: json['celik_kasamevcudu'],
+      kasa_defter_mevcudu: json['kasa_defter_mevcudu'],
+      fark: json['fark']
+    );
+  }
+
+}
+
+
---
+++ lib/services/cashCountingServices.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/cashCounting.dart';
+import '../models/shopOpeningControl.dart';
+
+Future<List<CashCounting>> parseJsonListOut(String jsonBody) async{
+  List<dynamic> responseList = jsonDecode(jsonBody);
+  List<CashCounting> returnList = [];
+  for (var element in responseList){
+    returnList.add(CashCounting.fromJson(element));
+  }
+  return returnList;
+}
+
+Future<List<CashCounting>> fetchCashCounting(String url) async {
+  final response = await http
+      .get(Uri.parse(url));
+  if (response.statusCode == 200) {
+    return parseJsonListOut(response.body);
+  } else {
+    throw Exception('Failed to load CashCounting List');
+  }
+}
+
+Future<CashCounting> fetchCashCounting2(String url) async {
+  final response = await http
+      .get(Uri.parse(url));
+  if (response.statusCode == 200) {
+    return CashCounting.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    throw Exception('Failed to load CashCounting');
+  }
+}
+
+Future<List<CashCounting>> fetchCashCounting3(String url) async {
+  final response = await http.get(Uri.parse(url));
+  if (response.statusCode == 200) {
+    List<dynamic> jsonResponse = json.decode(response.body);
+    List<CashCounting> cashCountings = jsonResponse.map((data) {
+      return CashCounting.fromJson(data);
+    }).toList();
+    return cashCountings;
+  } else {
+    throw Exception('Failed to load CashCounting List 2');
+  }
+}
+
+Future<CashCounting> createCashCounting(int shopCode, int? bs_id, int? pm_id, String savingDate, int kagit_para_sayimi, int madeni_para_sayimi, int poslar_toplami, int masraflar_tediyeler, int celik_kasa_mevcudu, int kasa_defter_mevcudu, int fark, String url) async {
+  final response = await http.post(Uri.parse(url),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, dynamic>
+      {
+        "magaza_kodu": shopCode,
+        "bs_id": bs_id,
+        "pm_id": pm_id,
+        "kayit_tarihi": savingDate,
+        "kagit_para_sayimi" : kagit_para_sayimi,
+        "madeni_para_sayimi" : madeni_para_sayimi,
+        "poslar_toplami" : poslar_toplami,
+        "masraflar_tediyeler" : masraflar_tediyeler,
+        "celik_kasa_mevcudu" : celik_kasa_mevcudu,
+        "kasa_defter_mevcudu" : kasa_defter_mevcudu,
+        "fark" : fark
+      }
+    ),
+  );
+  if (response.statusCode == 201) {
+    return CashCounting.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    throw Exception('Failed to create CashCounting');
+  }
+}
