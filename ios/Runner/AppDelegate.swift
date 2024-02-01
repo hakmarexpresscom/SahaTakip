28fc5d3c35b30b551f641cb80d08bb717a44e7d3
Elif Ünlü <elif.unlu@hakmarekspres.corp>
2024-02-01 16:49:52 +0300

    some updates

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,6 @@
 import UIKit
 import Flutter
 import GoogleMaps
-import Photos  // Eklediğimiz Photos framework'ünü import ettik.
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,21 +8,7 @@ import Photos  // Eklediğimiz Photos framework'ünü import ettik.
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Google Haritalar API anahtarınızı burada sağladığınızdan emin olun.
     GMSServices.provideAPIKey("AIzaSyCiElU8MOfQ8m9ht76QUiUvuhZyzVtiztA")
-
-    // Fotoğraf izni için kullanılan kısım.
-    if #available(iOS 10.0, *) {
-      PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
-        switch status {
-        case .authorized:
-          print("Access granted")
-        case .denied, .restricted, .notDetermined:
-          print("Access denied")
-        }
-      }
-    }
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
